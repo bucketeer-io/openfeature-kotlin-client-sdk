9e698b9a9690426562421c5dace3a7a50f51213e
duyhungtnn <duyhung71089@gmail.com>
2025-02-16 15:06:35 +0700

    test: Value to BKTValue

--- openfeatureprovider/src/main/java/io/bucketeer/openfeatureprovider/BKTValueExt.kt
+++ openfeatureprovider/src/main/java/io/bucketeer/openfeatureprovider/BKTValueExt.kt
@@ -12,15 +12,3 @@ fun BKTValue.toValue(): Value =
         is BKTValue.String -> Value.String(this.string)
         is BKTValue.Structure -> Value.Structure(this.structure.mapValues { it.value.toValue() })
     }
-
-fun Value.toBKTValue(): BKTValue =
-    when (this) {
-        is Value.Boolean -> BKTValue.Boolean(this.boolean)
-        is Value.List -> BKTValue.List(this.list.map { it.toBKTValue() })
-        Value.Null -> BKTValue.Null
-        is Value.Double -> BKTValue.Number(this.double)
-        is Value.Integer -> BKTValue.Number(this.integer.toDouble())
-        is Value.String -> BKTValue.String(this.string)
-        is Value.Structure -> BKTValue.Structure(this.structure.mapValues { it.value.toBKTValue() })
-        is Value.Date -> BKTValue.Number(this.date.time.toDouble())
-    }
---
+++ openfeatureprovider/src/test/java/io/bucketeer/openfeatureprovider/ValueExtTests.kt
@@ -0,0 +1,98 @@
+package io.bucketeer.openfeatureprovider
+
+import dev.openfeature.sdk.Value
+import io.bucketeer.sdk.android.BKTValue
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ValueExtTests {
+    @Test
+    fun test_ValueBoolean_toBKTValue() {
+        val value = Value.Boolean(true)
+        val bktValue = value.toBKTValue()
+        assertEquals(BKTValue.Boolean(true), bktValue)
+    }
+
+    @Test
+    fun test_ValueList_toBKTValue_withMultipleElements() {
+        val value =
+            Value.List(listOf(Value.String("test1"), Value.Double(42.0), Value.Boolean(true)))
+        val bktValue = value.toBKTValue()
+        assertEquals(
+            BKTValue.List(
+                listOf(
+                    BKTValue.String("test1"),
+                    BKTValue.Number(42.0),
+                    BKTValue.Boolean(true),
+                ),
+            ),
+            bktValue,
+        )
+    }
+
+    @Test
+    fun test_ValueList_toBKTValue_withEmptyList() {
+        val value = Value.List(emptyList())
+        val bktValue = value.toBKTValue()
+        assertEquals(BKTValue.List(emptyList()), bktValue)
+    }
+
+    @Test
+    fun test_ValueNull_toBKTValue() {
+        val value = Value.Null
+        val bktValue = value.toBKTValue()
+        assertEquals(BKTValue.Null, bktValue)
+    }
+
+    @Test
+    fun test_ValueNumber_Double_toBKTValue() {
+        val value = Value.Double(42.0)
+        val bktValue = value.toBKTValue()
+        assertEquals(BKTValue.Number(42.0), bktValue)
+    }
+
+    @Test
+    fun test_ValueNumber_Integer_toBKTValue() {
+        val value = Value.Integer(42)
+        val bktValue = value.toBKTValue()
+        assertEquals(BKTValue.Number(42.0), bktValue)
+    }
+
+    @Test
+    fun test_ValueString_toBKTValue() {
+        val value = Value.String("test")
+        val bktValue = value.toBKTValue()
+        assertEquals(BKTValue.String("test"), bktValue)
+    }
+
+    @Test
+    fun test_ValueStructure_toBKTValue_withMultipleEntries() {
+        val value =
+            Value.Structure(mapOf("key1" to Value.String("value1"), "key2" to Value.Double(42.0)))
+        val bktValue = value.toBKTValue()
+        assertEquals(
+            BKTValue.Structure(
+                mapOf(
+                    "key1" to BKTValue.String("value1"),
+                    "key2" to BKTValue.Number(42.0),
+                ),
+            ),
+            bktValue,
+        )
+    }
+
+    @Test
+    fun test_ValueStructure_toBKTValue_withEmptyStructure() {
+        val value = Value.Structure(emptyMap())
+        val bktValue = value.toBKTValue()
+        assertEquals(BKTValue.Structure(emptyMap()), bktValue)
+    }
+
+    @Test
+    fun test_ValueDate_toBKTValue() {
+        val date = java.util.Date()
+        val value = Value.Date(date)
+        val bktValue = value.toBKTValue()
+        assertEquals(BKTValue.Number(date.time.toDouble()), bktValue)
+    }
+}
